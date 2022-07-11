0f4d4a1996761c15f1a312051a4dee0166472623
Wilghaner <wilghener@gmail.com>
2022-07-11 11:26:46 -0300

    feat: navbar component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import * as C from './styles';
+import {TiThMenu} from "react-icons/ti";
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return(
+    <C.Container>
+      <Link to='/'>
+        <C.Logo> Movie-App </C.Logo>
+      </Link>
+      <TiThMenu 
+        size="34" 
+        color="#fff"
+        className='icon'
+      />
+    </C.Container>
+  )
+
+}
---
+++ src/components/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  background-color: #31B305;
+  height: 32px;
+  padding: 4px 8px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+`;
