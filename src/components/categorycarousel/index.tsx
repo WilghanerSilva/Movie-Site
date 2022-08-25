be6df33297b50db1367231cebe49840f9968a43f
Wilghaner <wilghener@gmail.com>
2022-08-25 14:49:45 -0300

    fix: resolve react-hooks/exhaustive-deps

--- src/components/categorycarousel/index.tsx
+++ src/components/categorycarousel/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as C from './styles';
 import { MoviePoster } from "../movieposter";
 import {MouseEvent, useEffect, useState} from 'react';
